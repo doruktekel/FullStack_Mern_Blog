ae8ca3f322e75cf0b32e8ee51f509a9016990e37
doruk.tekel <doruk.tekel@bilgeadam.com>
2024-08-25 19:44:51 +0300

    redux toolkit, and redux persist entagrated

---
+++ client/src/features/user/userSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInLoading: (state) => {
+      state.currentUser = null;
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const { signInFailure, signInLoading, signInSuccess } =
+  userSlice.actions;
+
+export default userSlice.reducer;
